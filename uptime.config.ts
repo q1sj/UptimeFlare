0c8dd50fd5bef2d5eb2d1053749c4423025fda3b
q1sj <51751541+q1sj@users.noreply.github.com>
2024-10-15 14:46:21 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -3,9 +3,9 @@ const pageConfig = {
   title: "lyc8503's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/q1sj', label: 'GitHub' },
+    { link: 'https://q1sj.cn/', label: 'Blog' },
+    { link: 'mailto:root@q1sj.cn', label: 'Email Me', highlight: true },
   ],
 }
 
@@ -19,13 +19,13 @@ const workerConfig = {
     // Example HTTP Monitor
     {
       // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
+      id: 'alist',
       // `name` is used at status page and callback message
-      name: 'My API Monitor',
+      name: 'Alist',
       // `method` should be a valid HTTP Method
-      method: 'POST',
+      method: 'GET',
       // `target` is a valid URL
-      target: 'https://example.com',
+      target: 'https://alist.q1sj.cn',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
